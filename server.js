781f7c4d91a92b4da9c92bf1c3cf49a77f9d277e
lancevd <olamosob@gmail.com>
2024-11-07 08:17:27 +0100

    server/reg

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const express = required("express")
+const express = require("express")
 
 const app = express();
 
 app.get("/",(req, res) =>{
-    console.log("Hello World") 
+    res.send("Hello world there")
 })
+
+app.post("/register", (res, req) {
+    req.body.email = req.body.email.trim();
+
+    if (!req.body.username){
+        errors.push("You must enter a username")
+    }
+})
+
+app.listen(5000)
