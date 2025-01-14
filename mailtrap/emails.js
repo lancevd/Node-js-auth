c0f2b835d6c3a6cbf4557cc1d95e51149585b993
lancevd <olamosob@gmail.com>
2025-01-14 14:04:00 +0100

    verification sent

--- mailtrap/emails.js
+++ mailtrap/emails.js
@@ -5,9 +5,8 @@ export const sendVerificationEmail = async (email, verificationToken) => {
   const recipient = [{ email }];
 
   try {
-    const response = await mailtrapClient.sendEmail({
-      from_email: sender.email,
-      from_name: sender.name,
+    const response = await mailtrapClient.send({
+      from: sender,
       to: recipient,
       subject: "Verify your email",
       html: VERIFICATION_EMAIL_TEMPLATE.replace(
