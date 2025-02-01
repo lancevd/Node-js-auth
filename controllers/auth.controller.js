28b778245092452dd2b4d91afc01603927f4bd25
lancevd <olamosob@gmail.com>
2025-02-01 07:01:59 +0100

    checkauth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -231,8 +231,23 @@ export const resend = (req, res) => {
 
 export const CheckAuth = (req, res) => {
   try {
-    
+    const user = User.findById(req.userID).select("-password");
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    res.status(200).json({
+      message: "User found",
+      success: true,
+      user, 
+    })
   } catch (error) {
-    
+    res.status(500).json({
+      message: error.message,
+      success: false,
+    });
   }
 }
